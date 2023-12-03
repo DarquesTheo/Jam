a02f735fc7b5a46b10b04cef97204fedd22592de
CB4fr2 <baptiste.camerlynck@epitech.eu>
2023-12-03 05:34:35 +0100

    add blood animation only on enemy

--- Weapon/bullet.gd
+++ Weapon/bullet.gd
@@ -4,14 +4,12 @@ const SPEED = 30.0
 
 @onready var mesh = $MeshInstance3D
 @onready var ray = $RayCast3D
-@onready var particles = $GPUParticles3D
+@onready var blood = $blood_particule
 
-var test = load("res://Enemy/BodyPart.gd")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += transform.basis * Vector3(0, 0, -SPEED) * delta
@@ -20,7 +18,7 @@ func _process(delta):
 		ray.enabled = false
 		if ray.get_collider().is_in_group("enemy"):
 			ray.get_collider().hit()
-			particles.emitting = true
+			blood.emitting = true
 			await get_tree().create_timer(1.0).timeout
 		queue_free()
 
